0610f33cbc4cdaf3a075af21abe7003d2f14c105
awkirin <awkirin@gmail.com>
2025-10-16 17:59:45 +0300

    update

--- home/.chezmoiscripts/run_onchange_before_0.sh
+++ home/.chezmoiscripts/run_onchange_before_0.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -eufo pipefail
 echo "${HOME}/.local/share/chezmoi/home/.chezmoiscripts/_global.sh"
-"${HOME}/.local/share/chezmoi/home/.chezmoiscripts/_global.sh"
+#"${HOME}/.local/share/chezmoi/home/.chezmoiscripts/_global.sh"
