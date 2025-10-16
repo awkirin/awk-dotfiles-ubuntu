61298d67f61cd7c67933f8a0d74473722df13c06
awkirin <awkirin@gmail.com>
2025-10-16 19:31:47 +0300

    update

--- home/.chezmoiscripts/run_onchange_before_install-packages.sh
+++ home/.chezmoiscripts/run_onchange_before_install-packages.sh
@@ -1,42 +1,41 @@
 #!/bin/bash
 set -eufo pipefail
 
+sudo apt update
+
 # deps
 deps=(
-  "curl"
-  "wget"
-  "git"
-  "jq"
-  "btop"
-  "bat"
-  "zsh"
-  "ansible"
-  "build-essential"
-  "fd-find"
-  "fzf"
-  "git-lfs"
-  "libfuse2"
-  "openssh-server"
-  "ripgrep"
-  "shellcheck"
-  "units"
-  "xclip"
-  "musl-tools"
-  "gdal-bin"
-  "libgeos-dev"
-  "libproj-dev"
-  "postgis"
-  "postgresql"
-  "qgis"
+  curl
+  wget
+  git
+  jq
+  btop
+  bat
+  zsh
+  ansible
+  build-essential
+  fd-find
+  fzf
+  git-lfs
+  libfuse2
+  openssh-server
+  ripgrep
+  shellcheck
+  units
+  xclip
+  musl-tools
+  gdal-bin
+  libgeos-dev
+  libproj-dev
+  postgis
+  postgresql
+  qgis
 )
-need_install=false
 for pkg in "${deps[@]}"; do
-  if ! dpkg -l | grep -q " $pkg "; then
-    need_install=true
-    break
-  fi
+    if apt list --installed 2>/dev/null | grep -q "^$pkg/"; then
+        echo "✓ $pkg уже установлен"
+    else
+        echo "📦 Устанавливаю $pkg..."
+        sudo apt install -y "$pkg"
+    fi
 done
-if $need_install; then
-  sudo apt update
-  sudo apt install -y "${deps[@]}"
-fi
