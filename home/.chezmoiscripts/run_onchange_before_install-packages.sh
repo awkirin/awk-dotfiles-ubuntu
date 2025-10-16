b026f8a554ae976232de83652d1c757b0fef72bb
awkirin <awkirin@gmail.com>
2025-10-16 18:54:44 +0300

    update

---
+++ home/.chezmoiscripts/run_onchange_before_install-packages.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+set -eufo pipefail
+
+# deps
+deps=(
+  "bat"
+  "build-essential"
+  "curl"
+  "fd-find"
+  "fzf"
+  "git"
+  "git-lfs"
+  "jq"
+  "libfuse2"
+  "openssh-server"
+  "ripgrep"
+  "shellcheck"
+  "units"
+  "btop"
+  "xclip"
+  "musl-tools"
+  "zsh"
+  "gdal-bin"
+  "libgeos-dev"
+  "libproj-dev"
+  "postgis"
+  "postgresql"
+  "qgis"
+)
+need_install=false
+for pkg in "${deps[@]}"; do
+  if ! dpkg -l | grep -q " $pkg "; then
+    need_install=true
+    break
+  fi
+done
+if $need_install; then
+  sudo apt update
+  sudo apt install -y "${deps[@]}"
+fi
